ee3ee3a9b66d6137868ced6792c80d33a5afab14
Devansh Tyagi <151578654+Devansh-Tyagi@users.noreply.github.com>
2024-01-10 03:32:26 +0530

    Update ticket generator.py

--- ticket generator.py
+++ ticket generator.py
@@ -1,18 +1,26 @@
+#A dictionary predefined to get the correct flight number when user inputs source and destination.
+dic={"AL1":"HARIDWAR TO DELHI","AL2":"DELHI TO LUCKNOW","BA1":"PATNA TO MUMBAI","DA1":"MUMBAI TO BANGLORE","JA4":"BANGLORE TO SHRIDI","KA2":"MUZZAFARNAGAR TO MEERUT"}
 #number of tickets one wants to make for a particular flight.
 num=int(input("how many tickets you wanna make"))
 base=num+101
 lst=[]
-i=101#ticket number
-airline="AL1"
-while int(i)<base:
+J=101#ticket number
+while J<base:
     source=input("enter source city")
     str2=source[0:3]
     dest=input("enter destination city")
     dest1=dest[0:3]
+    for i in dic:
+        st=dic[i]
+        ls=st.split()
+        if source in ls and dest in ls:
+            airline=i
+        else:
+            continue
     #format of ticket in str3 : example- AL1 : Har : Del : 101
-    str3=airline+':'+str2+':'+dest1+':'+str(i)
+    str3=airline+':'+str2+':'+dest1+':'+str(J)
     lst.append(str3)
-    i=i+1
+    J=J+1
 print(lst)
 
 
